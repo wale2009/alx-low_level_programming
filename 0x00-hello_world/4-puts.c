d8a63ce51ec85da9ad2d8870ecba7f0ed05e9402
wale2009 <adewaleadebayo202@gmail.com>
2023-07-13 17:29:52 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entry piont
+*
+* * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
