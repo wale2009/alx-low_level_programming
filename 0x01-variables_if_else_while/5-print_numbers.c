c8985248db223501719ac80bd5f45a2018d40e46
wale2009 <adewaleadebayo202@gmail.com>
2023-07-16 17:11:06 -0700

    task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - print single digit numbers
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
